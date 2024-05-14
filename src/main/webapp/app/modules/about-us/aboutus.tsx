7d88a5192a591c283edf13ab5d3ff9c0b04d0f79
tb0902 <tbruton417@gmail.com>
2024-05-14 17:49:13 -0400

    Finished about us page

--- src/main/webapp/app/modules/about-us/aboutus.tsx
+++ src/main/webapp/app/modules/about-us/aboutus.tsx
@@ -4,21 +4,21 @@ import React from 'react';
 export const TreyIcon = treyProps => (
   <div>
     <a href="#">
-      <img src="/content/images/trey.jpeg" alt="Trey" style={{ width: '330px' }} />
+      <img src="/content/images/trey2.JPG" alt="Trey" style={{ width: '330px', paddingTop: '5px' }} />
     </a>
   </div>
 );
 
 export const MichaelIcon = michaelProps => (
   <div>
-    <img src="/content/images/michael.jpeg" alt="Michael" style={{ width: '330px' }} />
+    <img src="/content/images/michael2.JPG" alt="Michael" style={{ width: '330px', paddingTop: '5px' }} />
   </div>
 );
 
 export const AnthonyIcon = anthonyProps => (
   <div>
     <a href="#">
-      <img src="/content/images/anthony.jpeg" alt="Anthony" style={{ width: '330px' }} />
+      <img src="/content/images/anthony2.JPG" alt="Anthony" style={{ width: '330px', paddingTop: '5px' }} />
     </a>
   </div>
 );
@@ -26,7 +26,7 @@ export const AnthonyIcon = anthonyProps => (
 export const ChazIcon = chazProp => (
   <div>
     <a href="#">
-      <img src="/content/images/chazAboutme.jpg" alt="Chaz" style={{ width: '330px' }} />
+      <img src="/content/images/chaz2.jpg" alt="Chaz" style={{ width: '330px', paddingTop: '5px' }} />
     </a>
   </div>
 );
@@ -34,7 +34,7 @@ export const ChazIcon = chazProp => (
 export const AliIcon = aliProps => (
   <div>
     <a href="#">
-      <img src="/content/images/ali.jpeg" alt="Ali" style={{ width: '330px' }} />
+      <img src="/content/images/ali2.JPG" alt="Ali" style={{ width: '330px', paddingTop: '5px' }} />
     </a>
   </div>
 );
@@ -42,9 +42,9 @@ export const AliIcon = aliProps => (
 const AboutUs = () => {
   return (
     <div className="about-section">
-      <h1>About Us</h1>
-      <p>Some text about who we are and what we do.</p>
-      <p>Resize the browser window to see that this page is responsive by the way.</p>
+      <h1>WilmFilm Development Team</h1>
+      <p>Tech Stack:</p>
+      <p>React.js(HTML, CSS, JavaScript & TypeScript); Spring Boot; Java; Axios; AWS S3; Docker</p>
 
       <h2 style={{ textAlign: 'center' }}>Team Bodleian</h2>
 
@@ -53,12 +53,13 @@ const AboutUs = () => {
           <div className="card">
             <ChazIcon />
             <div className="container">
-              <h2>Chaz Adams</h2>
+              <h2 className="flname">Chaz Adams</h2>
               <p className="title">Software Developer</p>
-              <p>
+              <p className="aliText">
                 Mastering the intricacies of full-stack development. With experience as an airborne infantryman in the Army, I bring strong
                 teamwork, adaptability, and problem-solving skills to the table.
               </p>
+              <div className="chazspace"></div>
               <p>
                 <a href="https://github.com/Chaz-Adams">
                   <button className="button">GitHub</button>
@@ -75,9 +76,14 @@ const AboutUs = () => {
           <div className="card">
             <AliIcon />
             <div className="container">
-              <h2>Ali Bangash</h2>
+              <h2 className="flname">Ali Bangash</h2>
               <p className="title">Software Developer</p>
-              <p>Docker</p>
+              <p className="aliText">
+                Embracing a journey of perpetual growth and evolution, I am dedicated to continually refining my expertise in software
+                engineering. Grounded in curiosity, adept listening, and effective communication, I approach each project with unwavering
+                determination, embracing challenges with a never-give-up spirit.
+              </p>
+              <div className="space"></div>
               <p>
                 <a href="https://github.com/ali-b14">
                   <button className="button">GitHub</button>
@@ -94,13 +100,14 @@ const AboutUs = () => {
           <div className="card">
             <TreyIcon />
             <div className="container">
-              <h2>Trey Bruton</h2>
+              <h2 className="flname">Trey Bruton</h2>
               <p className="title">Software Developer</p>
-              <p>
+              <p className="treyText">
                 Eager to learn and implement technical problem-solving skills to create change in the world. Serving the country as a 12N -
                 Heavy Construction Engineer, I plan to use the skills ingrained in me from my Army career in my future career as a Software
                 Developer.
               </p>
+              <div className="treyspace"></div>
               <p>
                 <a href="https://github.com/tb0902">
                   <button className="button">GitHub</button>
@@ -117,12 +124,13 @@ const AboutUs = () => {
           <div className="card bottom-card">
             <MichaelIcon />
             <div className="container">
-              <h2>Michael Scott</h2>
+              <h2 className="flname">Michael Scott</h2>
               <p className="title">Software Developer</p>
-              <p>
+              <p className="michaelText">
                 Passionate about transitioning into a role as a Software Engineer, leveraging my background in Electronic Engineering and
                 customer service experience to drive innovative solutions and enhance user experiences.
               </p>
+              <div className="michaelspace"></div>
               <p>
                 <a href="https://github.com/MichaelS0521">
                   <button className="button">GitHub</button>
@@ -139,10 +147,14 @@ const AboutUs = () => {
           <div className="card bottom-card">
             <AnthonyIcon />
             <div className="container">
-              <h2>Anthony Pearson</h2>
+              <h2 className="flname">Anthony Pearson</h2>
               <p className="title">Software Developer</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-
+              <p className="anthonyText">
+                {' '}
+                Former U.S. Navy computer network technician with 10 years of experience working with Linux/Unix, electronic circuitry and
+                computer hardware. I am a highly driven and goal oriented individual, currently enrolled in an intense Java software
+                development bootcamp.{' '}
+              </p>
               <p>
                 <a href="https://github.com/AnthonyP365">
                   <button className="button">GitHub</button>
